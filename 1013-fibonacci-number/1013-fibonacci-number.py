fac996fd30433ed0283205f0f6b39bb9613a6747
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-01-30 09:21:06 -0500

    Time: 37 ms (68.09%), Space: 17.7 MB (30.88%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,7 @@
+from functools import cache
 class Solution:
+    @cache
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-
+        if n <= 1:
+            return n
         return self.fib(n-1) + self.fib(n-2)
-        
