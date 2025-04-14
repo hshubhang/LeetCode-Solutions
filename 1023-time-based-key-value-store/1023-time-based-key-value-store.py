484d3686ea637c636ed5a04217c77a51be2f4228
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-04-14 15:55:54 -0400

    Time: 176 ms (11.41%), Space: 74.2 MB (85.05%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -1,3 +1,5 @@
+from sortedcontainers import SortedDict
+
 class TimeMap:
 
     def __init__(self):
@@ -5,22 +7,19 @@ class TimeMap:
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.timestamp_dict:
-            self.timestamp_dict[key] = []
-        self.timestamp_dict[key].append((timestamp, value))
+            self.timestamp_dict[key] = SortedDict()
+        self.timestamp_dict[key][timestamp] = value
+        
+      
     def get(self, key: str, timestamp: int) -> str:
         if key not in self.timestamp_dict:
             return ""
-        arr = self.timestamp_dict[key]
-        result = ""
-        left, right = 0, len(arr) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if arr[mid][0] <= timestamp:
-                result = arr[mid][1]
-                left = mid + 1
-            else:
-                right = mid - 1
-        return result
+        
+        it = self.timestamp_dict[key].bisect_right(timestamp)
+
+        if it == 0:
+            return ""
+        return self.timestamp_dict[key].peekitem(it - 1)[1]
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
 # obj.set(key,value,timestamp)
