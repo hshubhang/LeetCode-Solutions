6ffb9e9e7ad9925754b82c39d086e1d8fc5a2e29
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-06-12 15:46:43 -0700

    Time: 47 ms (36.45%), Space: 18 MB (58.55%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,25 +8,43 @@ class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         if not subRoot:
             return True
-        if not root:
-            return False
-        def isSameTree(node, subNode):
-                if not node and not subNode:
-                    return True
-                if not node and subNode or node and not subNode:
-                    return False
-                if node.val != subNode.val:
-                    return False
-                leftSide = isSameTree(node.left, subNode.left)
-                rightside = isSameTree(node.right, subNode.right)
-
-                return leftSide and rightside
-
+            
         if root.val == subRoot.val:
             if isSameTree(root, subRoot):
                 return True
+                
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-      
-
-
+        # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
         
+        def isSameTree(node, subNode):
+            if not node and not subNode:
+                return True
+            if not node and subNode or node and not subNode:
+                return False
+            if node.val != subNode.val:
+                return False
+            leftSide = isSameTree(node.left, subNode.left)
+            rightside = isSameTree(node.right, subNode.right)
+            
+            return leftSide and rightside
+        
+        if root.val == subRoot.val:
+            if isSameTree(root, subRoot):
+                return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+       
+
+    
