bfc4f47d72dbb140ab549bf06bc922e28b45a273
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-06-26 16:18:48 -0400

    Time: 19 ms (37.52%), Space: 22.7 MB (12.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,14 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        hashmap = {}
-        for i in strs:
-            word = ''.join(sorted(i))
-            if word not in hashmap:
-                hashmap[word] = []
-            hashmap[word].append(i)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashmap = defaultdict(list)
+        
+        for word in strs:
+            count = [0] * 26
+            for ch in word:
+                count[ord(ch) - ord("a")] += 1
+            key = tuple(count)
+            hashmap[key].append(word)
+        
+
         return list(hashmap.values())
-
-
