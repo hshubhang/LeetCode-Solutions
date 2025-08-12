3bb0ac76a205560790e90cc4338d7f49af4c957f
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-12 15:27:23 -0700

    Time: 5 ms (85.84%), Space: 19.1 MB (45.25%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -5,6 +5,6 @@ class Solution:
 
         frequency = Counter(list_s)
 
-        sorted_chars = sorted(frequency.keys(), key=lambda ch: frequency[ch], reverse=True)
+        sorted_chars = sorted(frequency.keys(), key= lambda ch: frequency[ch], reverse= True)
 
         return "".join(char * frequency[char] for char in sorted_chars)
