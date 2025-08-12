c6bb3ad0a9529d29f1cae8e580d016dd4bf608a3
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-12 15:13:05 -0700

    Time: 0 ms (100%), Space: 18 MB (21.28%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        hashmap = defaultdict(int)
+
+        parsed_string1 = s1.split(" ")
+        parsed_string2 = s2.split(" ")
+
+        for i in range(len(parsed_string1)):
+            hashmap[parsed_string1[i]] += 1
+        for j in range(len(parsed_string2)):
+            hashmap[parsed_string2[j]] += 1
+
+        return [word for word, count in hashmap.items() if count == 1]
