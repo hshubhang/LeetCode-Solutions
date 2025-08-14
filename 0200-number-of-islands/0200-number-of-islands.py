32ec04589a5a1d529bccaf601c1fae2ff8c86f11
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-14 15:11:37 -0700

    Time: 242 ms (66.08%), Space: 20.3 MB (57.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,21 +2,34 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        visited = [[False for j in range(len(grid[0]))] for i in range(len(grid))]
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        visited = [[False for j in range(cols)] for i in range(rows)]
+
         counter = 0
-        def dfs(i, j):
-            if (i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or visited[i][j] or grid[i][j] == "0"):
+        def dfs (i, j):
+            if i < 0 or i >= rows or j < 0 or j >= cols or grid[i][j] == "0" or visited[i][j] == True:
                 return
+            
             visited[i][j] = True
 
-            dfs(i+1, j)
             dfs(i-1, j)
+            dfs(i+1, j)
             dfs(i, j+1)
-            dfs(i, j-1)
+            dfs(i, j - 1)
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1" and not visited[i][j]:
-                    dfs(i, j)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and not visited[r][c]:
+                    dfs(r,c)
                     counter += 1
+
         return counter
+        
+
+
+        
+
+
