a99be9c728fefcfde22acb03e196bb416cea86fd
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-14 19:10:32 -0700

    Time: 7 ms (25.34%), Space: 17.6 MB (99.24%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,30 +1,45 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue = deque()
+        if not grid:
+            return -1
         rows = len(grid)
         cols = len(grid[0])
-        minutes = 0
+
+        visited = [[False for c in range(cols)]for r in range(rows)]
+        directions = [(-1,0), (1,0), (0, -1), (0, 1)]
+        queue = deque()
         fresh = 0
-        directions = [(1,0), (0,1), (-1, 0), (0, -1)]
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i,j))
-                elif grid[i][j] == 1:
+        time = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and not visited[r][c]:
                     fresh += 1
 
-                print("Initial Oranges:", queue)
-                print("Fresh Oranges: ", fresh)
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                    visited[r][c] = True
 
-        while queue and fresh > 0:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
+        while queue:
+            infected_this_minute = False
+            layer_size = len(queue)
+            for _ in range(layer_size):
+
+                row, col = queue.popleft()
                 for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2  # Rot the orange
+                    nr = row + dr
+                    nc = col + dc
+
+                    if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
                         fresh -= 1
                         queue.append((nr, nc))
-            minutes += 1
+                        infected_this_minute = True
+                        visited[nr][nc] = True
+            if infected_this_minute:
+                time += 1
 
-        return minutes if fresh == 0 else -1
+        return time if fresh == 0 else -1
+ 
+            
