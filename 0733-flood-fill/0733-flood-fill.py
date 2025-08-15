aaf892541c29b2e6a944fb838177da178199533f
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-15 13:41:12 -0700

    Time: 0 ms (100%), Space: 18.1 MB (26.9%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,13 +1,15 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        if not image:
-            return []
+        old = image[sr][sc]
+        if old == color:
+            return image
         rows = len(image)
         cols = len(image[0])
         visited = [[False for c in range(cols)]for r in range(rows)]
         queue = deque()
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         queue.append((sr, sc))
+        image[sr][sc] = color
         visited[sr][sc] = True
         
         while queue:
@@ -16,10 +18,8 @@ class Solution:
                 nr = row + dr
                 nc = col + dc
 
-                if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and image[nr][nc] == image[sr][sc]:
+                if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and image[nr][nc] == old:
                     image[nr][nc] = color
                     queue.append((nr, nc))
                     visited[nr][nc] = True
-
-        image[sr][sc] = color
         return image
