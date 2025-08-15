71ee1c9c5874ebd51091775b0f7f3c7f2bb26aa4
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-15 13:47:20 -0700

    Time: 1 ms (24.96%), Space: 18.3 MB (13.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,21 +5,27 @@ class Solution:
             return image
         rows = len(image)
         cols = len(image[0])
-        visited = [[False for c in range(cols)]for r in range(rows)]
-        queue = deque()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        queue.append((sr, sc))
-        image[sr][sc] = color
-        visited[sr][sc] = True
-        
-        while queue:
-            row, col = queue.popleft()
-            for dr, dc in directions:
-                nr = row + dr
-                nc = col + dc
 
-                if 0 <= nr < rows and 0 <= nc < cols and not visited[nr][nc] and image[nr][nc] == old:
-                    image[nr][nc] = color
-                    queue.append((nr, nc))
-                    visited[nr][nc] = True
+        visited = [[False for c in range(cols)] for r in range(rows)]
+
+
+        def dfs(i, j, color):
+
+            if i < 0 or i >= rows or j < 0 or j >= cols or visited[i][j] or image[i][j] != old:
+                return
+
+            visited[i][j] = True
+            image[i][j] = color
+
+            dfs(i-1, j, color)
+            dfs(i+1, j , color)
+            dfs(i, j-1, color)
+            dfs(i, j+1, color)
+
+
+
+        dfs(sr, sc, color)
+
         return image
+
+        
