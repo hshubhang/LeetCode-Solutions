c6f657eddee422379e52f2a2bc63de8965ab61b6
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-08-20 11:21:01 -0700

    Time: 287 ms (95.35%), Space: 58.3 MB (46.84%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -1,35 +1,35 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        
         res = []
+
         minheap = []
         heapq.heapify(minheap)
-        
+
         available_tasks = []
 
-        for idx, (eneque_time, processing_time) in enumerate(tasks):
-            available_tasks.append((eneque_time, processing_time, idx))
+        for idx, (enqueue_time, process_time) in enumerate(tasks):
+            available_tasks.append((enqueue_time, process_time, idx))
+
         available_tasks.sort()
-        
-        number_of_tasks = len(tasks)
+        n = len(tasks)
         time = 0
         i = 0
-        while len(res) < number_of_tasks:
 
-            if not minheap and i < number_of_tasks and time < available_tasks[i][0]:
+        while len(res) < n:
+            if not minheap and i < n and time < available_tasks[i][0]:
                 time = available_tasks[i][0]
 
-            while i < number_of_tasks and available_tasks[i][0] <= time:
-                et, pt, idx = available_tasks[i]
-                heapq.heappush(minheap, (pt, idx))
-                i+=1
+            while i < n and available_tasks[i][0] <= time:
+                eneque_time, processing_time, idx = available_tasks[i]
+                heapq.heappush(minheap, (processing_time, idx))
+                i += 1
 
             if minheap:
-                pt, idx = heapq.heappop(minheap)
+                processing_time, idx = heapq.heappop(minheap)
                 res.append(idx)
-                time += pt
-        
-        return res
+                time += processing_time
             
+        return res
+
 
 
