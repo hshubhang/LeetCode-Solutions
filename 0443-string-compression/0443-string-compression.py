60daaaec867736ae9e926dd009e405d8d38d1415
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2025-11-06 13:13:23 -0800

    Time: 2 ms (45.52%), Space: 17.9 MB (45.66%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,6 +1,6 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        s = ""
+        S = []
         count = 0
         left = 0
         right = 0
@@ -12,21 +12,26 @@ class Solution:
                 right += 1
             elif chars[right] != chars[left]:
                 if count == 1:
-                    s += f"{chars[left]}"
-                else:
-                    s += f"{chars[left]}{count}" 
+                    S.append(chars[left])
+                else: 
+                    S.append(chars[left])
+                    S.append(str(count))
                 left = right
                 count = 0
             
         if count == 1:
-            s += f"{chars[left]}"
+            S.append(chars[left])
         else:
-            s += f"{chars[left]}{count}" 
+            S.append(chars[left])
+            S.append(str(count))
 
-        for i in range(len(s)):
-            chars[i] = s[i]
+        new_str = "".join(S)
+        print(new_str)
+
+        for i in range(len(new_str)):
+            chars[i] = new_str[i]
         
-        return len(s)
+        return len(new_str)
                 
 
         
