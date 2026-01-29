ca9e2b44519eea150c09112156d4c038226eaa16
Shubhang Vinay Hasabnis <48579562+hshubhang@users.noreply.github.com>
2026-01-29 16:07:26 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.51%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
+        
+
+        seen = set(startGene)
+
+        def neighboring_genes(node):
+            ans = []
+            for i in range(8):
+                for new_char in ["A", "C", "G", "T"]:
+                    if new_char != node[i]:
+                        new_string = node[:i] + new_char + node[i+1:]
+                        if new_string in bank:
+                            ans.append(new_string)
+
+            
+            return ans
+        
+        queue = deque([startGene])
+        steps = 0
+        while queue:
+            nodes_at_curr_level = len(queue)
+
+            for _ in range(nodes_at_curr_level):
+                
+                node = queue.popleft()
+
+                if node == endGene:
+                    return steps
+
+                
+                for neighbor in neighboring_genes(node):
+                    if neighbor not in seen and neighbor in bank:
+                        seen.add(neighbor)
+                        queue.append(neighbor)
+
+            
+            steps += 1
+
+        return -1
+    
+
+
+
